6615df0e99e1bff6f2e3d77467d8ca9c7e4bf256
Sungjin Ahn <46511987+SDSTony@users.noreply.github.com>
2021-01-20 20:47:01 +0900

    add gan colorization dataset
    victorian400

--- PL_data_loader.py
+++ PL_data_loader.py
@@ -42,7 +42,8 @@ file_id_list = [
 ]
 
 file_id_dic = {'FaceMaskDetection':'1pJtohTc9NGNRzHj5IsySR39JIRPfkgD3',
-              'COVIDTimeSeries':'1B36Djko-f0LtVxSaBEVYosLTTsu0qtsh'}
+              'COVIDTimeSeries':'1B36Djko-f0LtVxSaBEVYosLTTsu0qtsh',
+              'GAN-Colorization':'1tPNnwPISjPaC1nDZ1QpXcJAQCFmeO3Uv'}
 
 destination = '' ### YOUR_DOWNLOAD_PATH
 # os.makedirs(destination, exist_ok=True)
