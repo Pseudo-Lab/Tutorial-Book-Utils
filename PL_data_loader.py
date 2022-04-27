708024eb22aa3c121586896f0117f94a8cbe574b
Sungjin Ahn <46511987+SDSTony@users.noreply.github.com>
2022-04-27 17:21:30 +0900

    resolves issue #2
    resolves #2

--- PL_data_loader.py
+++ PL_data_loader.py
@@ -1,78 +1,27 @@
-# taken from this repo: https://github.com/nsadawi/Download-Large-File-From-Google-Drive-Using-Python/
-import requests
-import os
-import os.path as pth
-# from multiprocessing import Pool
-# from functools import partial
-# from tqdm.notebook import tqdm
-import zipfile
-import argparse
+# taken from this repo: https://stackoverflow.com/questions/38511444/python-download-files-from-google-drive-using-url
 
-def download_file_from_google_drive(id_, destination):
-    URL = "https://docs.google.com/uc?export=download"
+import gdown
 
-    session = requests.Session()
-    response = session.get(URL, params = { 'id' : id_ }, stream = True)
-    token = get_confirm_token(response)
-    if token:
-        params = { 'id' : id_, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-        
-    basename = response.headers['Content-Disposition'].split(';')[1].split('filename=')[1].replace('\"', '')
-    full_dst_filenname = pth.join(destination, basename)
-    save_response_content(response, full_dst_filenname)
-    return full_dst_filenname
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-
-file_id_list = [
-    '1pJtohTc9NGNRzHj5IsySR39JIRPfkgD3', #Pseudo Lab Drive
-]
+file_destinations = {'FaceMaskDetection':'Face Mask Detection.zip',
+              'COVIDTimeSeries':'COVIDTimeSeries.zip',
+              'GAN-Colorization':'Victorian400-GAN-colorization-data.zip',
+              'NLP-QG':'CoNLL+BEA_corrected_essays.pkl'}
 
 file_id_dic = {'FaceMaskDetection':'1pJtohTc9NGNRzHj5IsySR39JIRPfkgD3',
               'COVIDTimeSeries':'1B36Djko-f0LtVxSaBEVYosLTTsu0qtsh',
               'GAN-Colorization':'1tPNnwPISjPaC1nDZ1QpXcJAQCFmeO3Uv',
               'NLP-QG':'1dPa5iLEv6WqeHZaYhNLPw5q_i5IGJzU9'}
 
-destination = '' ### YOUR_DOWNLOAD_PATH
-# os.makedirs(destination, exist_ok=True)
-
-filename_list = []
-
-
-
+def download_file_from_google_drive(id_, destination):
+    url = f'https://drive.google.com/uc?id={id_}'
+    output = destination
+    gdown.download(url, output, quiet=False)
+    print(f'{output} download complete!')
+    
 parser = argparse.ArgumentParser(description = 'data loader for PseudoLab Tutorial Book')
 
 parser.add_argument('--data', type = str, help = 'key for selecting data')
 
 args = parser.parse_args()
 
-
-filename = download_file_from_google_drive(id_=file_id_dic[args.data], destination=destination)
-print('{} is done!'.format(filename))
-filename_list.append(filename)
-
-# ### Use single process
-# for file_id in file_id_list:
-#     filename = download_file_from_google_drive(id_=file_id, destination=destination)
-#     print('{} is done!'.format(filename))
-#     filename_list.append(filename)
-
-### If you want to download more faster
-# download_func = partial(download_file_from_google_drive, destination=destination)
-# with Pool(4) as pool:
-#     for i, filename in tqdm(enumerate(pool.imap_unordered(download_func, file_id_list)), total=len(file_id_list)):
-#         print('{} is done!'.format(filename))
-#         filename_list.append(filename)
+download_file_from_google_drive(id_=file_id_dic[args.data], destination=file_destinations[args.data])
