f96848408c8fd57fe7bbca0554961288b029b0ea
Tony (Sungjin) Ahn <46511987+SDSTony@users.noreply.github.com>
2023-06-11 23:20:15 +0900

    Update FaceMaskDetection path

--- PL_data_loader.py
+++ PL_data_loader.py
@@ -8,7 +8,7 @@ file_destinations = {'FaceMaskDetection':'Face Mask Detection.zip',
               'GAN-Colorization':'Victorian400-GAN-colorization-data.zip',
               'NLP-QG':'CoNLL+BEA_corrected_essays.pkl'}
 
-file_id_dic = {'FaceMaskDetection':'1pJtohTc9NGNRzHj5IsySR39JIRPfkgD3',
+file_id_dic = {'FaceMaskDetection':'16Gzn1w38yZLJpzmzKcRIRveFeHZtCEr7',
               'COVIDTimeSeries':'1B36Djko-f0LtVxSaBEVYosLTTsu0qtsh',
               'GAN-Colorization':'1dZxoBIWmbuF-Oy_XZoS1z9EjPJwkTmy6',
               'NLP-QG':'1dPa5iLEv6WqeHZaYhNLPw5q_i5IGJzU9'}
